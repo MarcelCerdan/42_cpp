b1b915fcbd0676c0596fbfe9d9e189ecdc0b0fe8
Maxime Thibault <mthibaul@z1r4p3.42lyon.fr>
2023-08-23 17:39:19 +0200

    Completed cpp05-ex01

---
+++ 05/ex01/Bureaucrat.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mthibaul <mthibaul@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 11:23:00 by mthibaul          #+#    #+#             */
+/*   Updated: 2023/08/22 11:23:00 by mthibaul         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : name("Karen"), grade(150) { return ; }
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : name(name) {
+
+	if (grade > 150)
+		throw GradeTooLowException();
+	else if (grade < 1)
+		throw GradeTooHighException();
+	else
+		this->grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) {
+
+	*this = other;
+}
+
+Bureaucrat::~Bureaucrat() { return ; }
+
+std::string Bureaucrat::getName() const {
+
+	return (this->name);
+}
+
+int Bureaucrat::getGrade() const {
+
+	return (this->grade);
+}
+
+void Bureaucrat::increment() {
+
+	this->grade--;
+}
+
+void Bureaucrat::decrement() {
+
+	this->grade++;
+}
+
+void Bureaucrat::signForm(Form *form) {
+
+	if (this->grade <= form->getGradeToSign())
+		std::cout << this->name << " signed " << form->getName() << std::endl;
+	else
+	std::cout << this->name << " signed " << form->getName()
+			<< "because is grade is too low." << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other) {
+
+	if (this != &other)
+		this->grade = other.grade;
+	return (*this);
+}
+
+std::ostream	&operator<<(std::ostream &os, Bureaucrat const &b) {
+
+	os << b.getName() << ", bureaucrat grade " << b.getGrade() << std::endl;
+	return os;
+}
---
+++ 05/ex01/Form.hpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mthibaul <mthibaul@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 16:40:00 by mthibaul          #+#    #+#             */
+/*   Updated: 2023/08/23 16:40:00 by mthibaul         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form {
+
+	private:
+
+	std::string	const name;
+	bool		isSigned;
+	int const	gradeToSign;
+	int const	gradeToExecute;
+
+	public:
+
+	Form(void);
+	Form(std::string name, int grSign, int grExec);
+	Form(const Form &other);
+	~Form(void);
+
+	std::string getName() const;
+	int 		getGradeToSign() const;
+	int 		getGradeToExec() const;
+	bool		getIsSigned() const;
+	void		beSigned(Bureaucrat *b);
+
+	Form &operator=(const Form &other);
+
+	class	GradeTooHighException : public std::exception {
+
+	public:
+
+		const char *what() const throw() {
+			return ("Error : form's grade is too high !");
+		}
+	};
+
+	class	GradeTooLowException : public std::exception {
+
+	public:
+
+		const char *what() const throw() {
+			return ("Error : form's grade is too low !");
+		}
+	};
+};
+
+std::ostream	&operator<<(std::ostream &os, Form const &form);
+
+#endif
